e45f045f3e670d5d02cd4406d4acd0413e0c1646
hieuTran <tranminhhieu291104@gmail.com>
2025-07-08 20:26:56 +0700

    move JTW form authService to commonService

--- src/main/java/com/kane/account/AccountApplication.java
+++ src/main/java/com/kane/account/AccountApplication.java
@@ -3,13 +3,14 @@ package com.kane.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.kane.account", "com.kane.common"})
 @EnableFeignClients
 public class AccountApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AccountApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(AccountApplication.class, args);
+  }
 }
--- src/main/java/com/kane/account/controller/AccountController.java
+++ src/main/java/com/kane/account/controller/AccountController.java
@@ -1,26 +1,36 @@
 package com.kane.account.controller;
 
-
 import com.kane.account.infra.client.AuthClient;
-import com.kane.common.dto.request.SignUpRequest;
+import com.kane.common.dto.request.SignInRequest;
+import com.kane.common.dto.response.SignInResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
 
-    private final AuthClient authClient;
+  private final AuthClient authClient;
 
-    @PostMapping
-    ResponseEntity<?> test () {
-        SignUpRequest request = new SignUpRequest();
-        request.setName("Test");
-        request.setUsername("testuser");
-        request.setPassword("123456");
-        var res = authClient.auth(request);
-        return ResponseEntity.ok(res.getBody());
+  @PostMapping("/signIn")
+  public ResponseEntity<SignInResponse> signIn(@RequestBody SignInRequest request) {
+    try {
+      SignInResponse response = authClient.signIn(request);
+      return ResponseEntity.ok(response);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(null);
     }
+  }
+
+  @GetMapping("/call-auth")
+  public ResponseEntity<String> callAuth() {
+    String response = authClient.hello();
+    return ResponseEntity.ok("Account gọi auth: " + response);
+  }
+
+  @GetMapping("/test")
+  public ResponseEntity<String> test() {
+    return ResponseEntity.ok("Account gọi auth: ");
+  }
 }
